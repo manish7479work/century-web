fe4e541288e201d9557ef36f9b6f00726675406c
manish7479work <manish.kumar@alphadna.ai>
2025-05-22 14:39:04 +0530

    fix: data grid id

--- src/components/Form/MyDataGrid.jsx
+++ src/components/Form/MyDataGrid.jsx
@@ -7,7 +7,8 @@ export default function MyDataGrid({ rows, columns }) {
     <DataGrid
       rows={rows}
       columns={columns}
-      getRowId={(row) => row.id ?? row?.phone ?? row?.emp_code}
+      // getRowId={(row) => row.id ?? row?.phone ?? row?.emp_code ?? row?.pno}
+      getRowId={(row) => row.id || row.pno || row.phone || row.emp_code}
       showToolbar
       slots={{ toolbar: GridToolbar }}
       pagination
